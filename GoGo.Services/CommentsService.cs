f3e2fa01d3314d4395ad8a44fe2009b66ac23bba
nnikolova91 <n.nikolova.it@gmail.com>
2019-01-02 20:40:32 +0200

    moving services to separate project

--- GoGo.Services/CommentsService.cs
+++ GoGo.Services/CommentsService.cs
@@ -1,10 +1,5 @@
-﻿using GoGo.Data;
-using GoGo.Models;
+﻿using GoGo.Models;
 using GoGo.Services.Contracts;
-using ViewModels;
-using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GoGo.Data.Common;
